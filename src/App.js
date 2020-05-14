8e090e2ad087d097459530e07c7e3412d1d50d87
Lawal Oladotun <oladotunlawal7@gmail.com>
2020-05-14 16:27:08 +0100

    Removed app css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import Intro from './components/Intro/Intro'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
