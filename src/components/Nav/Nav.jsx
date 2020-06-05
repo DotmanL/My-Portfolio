67140201b5425f18907cdc184aa8c52625d0e8d3
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-06-05 16:31:56 +0100

    nav menu fix and ui fixes

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -16,13 +16,13 @@ return(
       
         <ScrollTo>
       {({ scroll }) => (
-        <NavLinks onClick={() => scroll({ y: 930 })}>Skills</NavLinks>
+        <NavLinks onClick={() => scroll({ y: 1020 })}>Skills</NavLinks>
           )}
         </ScrollTo>
 
         <ScrollTo>
       {({ scroll }) => (
-        <NavLinks onClick={() => scroll({ y: 1330 })}>Projects</NavLinks>
+        <NavLinks onClick={() => scroll({ y: 1530 })}>Projects</NavLinks>
           )}
         </ScrollTo>
 
@@ -49,13 +49,13 @@ return(
       
         <ScrollTo>
       {({ scroll }) => (
-        <NavmLinks onClick={() => scroll({ y: 930 })}>Skills</NavmLinks>
+        <NavmLinks onClick={() => scroll({ y: 1020 })}>Skills</NavmLinks>
           )}
         </ScrollTo>
 
         <ScrollTo>
       {({ scroll }) => (
-        <NavmLinks onClick={() => scroll({ y: 1330 })}>Projects</NavmLinks>
+        <NavmLinks onClick={() => scroll({ y: 1530 })}>Projects</NavmLinks>
           )}
         </ScrollTo>
 
