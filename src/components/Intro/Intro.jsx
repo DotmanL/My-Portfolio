1e706480094b91bd6897e5fe8ec20dbc3053e1a0
Lawal Oladotun <oladotunlawal7@gmail.com>
2020-06-11 14:04:31 +0100

    Fix

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -37,7 +37,7 @@ const Intro = () => {
                 
                 strings={[
                     'React Developer',
-                    'Aspiring FullStack Developer']}
+                    'FullStack Developer (MERN Stack']}
                     typeSpeed={90}
                     backSpeed={50}
                     loop >
